2a273104374be621e08b60654590b1fc1cedc225
Diego <ogeid.csilva@gmail.com>
2022-06-06 17:44:17 -0300

    Exercício 5 - Operadores lógicos em JAVA

---
+++ src/br/com/generation/lista/Ex1.java
@@ -0,0 +1,23 @@
+package br.com.generation.lista;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(new Locale("en", "US"));
+		double nota1, nota2, nota3, media;
+		Scanner leia = new Scanner(System.in);
+		System.out.println("Digite o valor da nota 1: ");
+		nota1 = leia.nextDouble();
+		System.out.println("Digite o valor da nota 2: ");
+		nota2 = leia.nextDouble();
+		System.out.println("Digite o valor da nota 3: ");
+		nota3 = leia.nextDouble();
+		media = (nota1*2 + nota2*3 +nota3*5) /10;
+		System.out.println("A sua média final é " +media);
+		
+	}
+
+}
